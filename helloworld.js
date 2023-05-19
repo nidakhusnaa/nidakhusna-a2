c27082054c2002697c3925721ebe2ad97f91d2da
nidakhusnaa <126132626+nidakhusnaa@users.noreply.github.com>
2023-05-19 09:51:36 +0700

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,19 @@
+document.writeln("Hello World");
+document.writeln("</br>");
+
+var fullName = "Nida Khusna Alfarisa";
+var firstName ="Nida";
+var middleName ="Khusna";
+var lastName ="Alfarisa";
+
+document.writeln("Nama lengkap : " + fullName);
+document.writeln("</br>");
+document.writeln("Nama depan : " + firstName);
+document.writeln("</br>");
+document.writeln("Nama tengah : " + middleName);
+document.writeln("</br>");
+document.writeln("Nama akhir : " + lastName);
+document.writeln("</br>");
+
+document.writeln("</br>");
+document.writeln("Nama : Nida");
